d8f681cd9c453580b4a2fa20515518cb330740b1
Aleksandr Bogdanov <basik0204@gmail.com>
2022-05-12 13:02:47 +0300

    Initial Commit

---
+++ TrackList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrackList
+//
+//  Created by Aleksandr Bogdanov on 12.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
